fe03bae1fccd8c2c4463a47f92c192f6ef6d66f6
jaeC <thingbaijamjessikhuman@gmail.com>
2022-05-22 19:14:07 +0530

    update 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -23,8 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
- 
-
   final String title;
 
   @override
@@ -32,19 +32,86 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
- 
-
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
-     
-      body: Column(children: [
-
-      ],)
-     
-    );
+        backgroundColor: Colors.grey[300],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "hello !",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            ),
+            const SizedBox(height: 10),
+            const Text("this is a SAMPLE login PAGE"),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 26),
+              child: Container(
+                padding: const EdgeInsets.only(left: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white)),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: "E-mail"),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 26),
+              child: Container(
+                padding: EdgeInsets.only(left: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white)),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: "password"),
+                  obscureText: true,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.00),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Text(
+                  "sign in",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const Text(
+                  "Not a Member ?",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  " REGISTER NOW",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.blue),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
